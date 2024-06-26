0ad172c02ae639260c3242d89e99013c82e236c7
lexxsh <john8538@naver.com>
2024-06-26 23:59:34 +0900

    0626-카카오 로그인 수정 sdk - rest api

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { createMemoryRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home'
 import KakaoLogin from './Pages/KakaoLogin'
 import KakaoRedirectHandler from './Pages/KakaoRedirectHandler'
+
+const ROUTE_PATH = {
+  HOME: '/',
+  LOGIN: '/login',
+  OAUTH: '/oauth',
+}
+
+const router = createMemoryRouter([
+  {
+    path: ROUTE_PATH.HOME,
+    element: <Home />,
+  },
+  {
+    path: ROUTE_PATH.LOGIN,
+    element: <KakaoLogin />,
+  },
+  {
+    path: ROUTE_PATH.OAUTH,
+    element: <KakaoRedirectHandler />,
+  },
+])
+
 function App() {
   console.log(import.meta.env.VITE_KAKAO_JS_SDK_KEY)
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<KakaoLogin />} />
-          <Route path="/oauth" element={<KakaoRedirectHandler />} />
-        </Routes>
-      </Router>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/Pages/KakaoLogin.tsx
+++ src/Pages/KakaoLogin.tsx
@@ -5,17 +5,18 @@ declare global {
 }
 
 const KakaoLogin = () => {
-  const handleKakaoLogin = () => {
-    window.Kakao.Auth.authorize({
-      redirectUri: 'http://localhost:5173/oauth',
-      scope: 'profile_nickname,profile_image',
-    })
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_JS_SDK_KEY
+  const REDIRECT_URI = 'http://localhost:5173/oauth'
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+
+  const loginHandler = () => {
+    window.location.href = link
   }
 
   return (
-    <div>
-      <button onClick={handleKakaoLogin}>카카오 로그인</button>
-    </div>
+    <button type="button" onClick={loginHandler}>
+      로그인 하기
+    </button>
   )
 }
 
