3c367ee0c60c9ecf9d50196e268c57070e45f610
lexxsh <john8538@naver.com>
2024-08-19 01:56:04 +0900

    캘린더 수정

--- src/Components/Schedule/schedule.tsx
+++ src/Components/Schedule/schedule.tsx
@@ -22,9 +22,7 @@ const Schedule: React.FC<{ date: Date | null; events: any[] }> = ({
         {events.length > 0 ? (
           events.map((event, index) => (
             <div key={index}>
-              <div className="Schedule-title">
-                {event.summary.replace(/[^\w\s]/gi, '')}
-              </div>
+              <div className="Schedule-title">{String(event.summary)}</div>
               <div className="Schedule-time">
                 {new Date(event.start.dateTime).toLocaleTimeString([], {
                   hour: '2-digit',
