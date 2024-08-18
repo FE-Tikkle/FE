d2857f2194ba79a3d6066b4bb0eb54337c815130
lexxsh <john8538@naver.com>
2024-08-19 02:12:29 +0900

    api 연결 전 커밋

--- src/Components/Schedule/schedule.tsx
+++ src/Components/Schedule/schedule.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './schedule.css'
 
-// 선택된 날짜와 이벤트를 받아서 일정 표시
 const Schedule: React.FC<{ date: Date | null; events: any[] }> = ({
   date,
   events,
@@ -12,16 +11,17 @@ const Schedule: React.FC<{ date: Date | null; events: any[] }> = ({
 
   return (
     <div className="Schedule">
-      <div className="Schedule-maincontainer">
-        <div className="Schedule-main2conatainer">
-          <div className="Schedule-recnet-icon"></div>
-          <div className="Schedule-date">
-            {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}일
-          </div>
-        </div>
-        {events.length > 0 ? (
-          events.map((event, index) => (
-            <div className="Schedule-key" key={index}>
+      {events.length > 0 ? (
+        events.map((event, index) => (
+          <div className="Schedule-maincontainer" key={index}>
+            <div className="Schedule-main2conatainer">
+              <div className="Schedule-recnet-icon"></div>
+              <div className="Schedule-date">
+                {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}
+                일
+              </div>
+            </div>
+            <div className="Schedule-key">
               <div className="Schedule-title">{String(event.summary)}</div>
               <div className="Schedule-time">
                 {new Date(event.start.dateTime).toLocaleTimeString([], {
@@ -35,11 +35,19 @@ const Schedule: React.FC<{ date: Date | null; events: any[] }> = ({
                 })}
               </div>
             </div>
-          ))
-        ) : (
+          </div>
+        ))
+      ) : (
+        <div className="Schedule-maincontainer">
+          <div className="Schedule-main2conatainer">
+            <div className="Schedule-recnet-icon"></div>
+            <div className="Schedule-date">
+              {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}일
+            </div>
+          </div>
           <div className="Schedule-title">일정이 없습니다.</div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
