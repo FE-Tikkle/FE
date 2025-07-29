c148f9359a1a93f2355a2bd64a41afb368d02190
yewonee <hyewon8685@naver.com>
2025-07-29 21:27:32 +0900

    refactor: footer 영역 rem 기반 반응형 스타일로 리팩토링

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -14,10 +14,7 @@ const Footer: React.FC = () => {
       <div className="Footer-main">
         <div className="Footer-Container">
           <div className="Footer-Logo">2024 티끌. AII RIGHT RESERVED.</div>
-          <div className="Footer-text">
-            <div>service.tikkeul@gmail.com</div>
-            <div className="Footer-address">인천광역시 미추홀구 인하로 100</div>
-          </div>
+        
         </div>
         <div className="Footer-up" onClick={scrollToTop}>
           <img src="img/up2.svg" alt="Up" />
